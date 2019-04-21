b70f75dc74d2a6d9c56132539a2ba58d56d86065
Charles Chan <charleswhchan@users.noreply.github.com>
2019-04-20 22:28:06 -0700

    CC - Add example for section 3.2

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # dwave-qubo-ocean-examples
-Implementation of QUBO examples from [A Tutorial on Formulating and Using QUBO Models](https://arxiv.org/pdf/1811.11538.pdf) using [D-Wave Ocean SDK](https://github.com/dwavesystems/dwave-ocean-sdk)
+Implement examples from [A Tutorial on Formulating and Using QUBO Models](https://arxiv.org/pdf/1811.11538.pdf) using [D-Wave Ocean SDK](https://github.com/dwavesystems/dwave-ocean-sdk)
 
 - [Section 2: Illustrative Examples and Definitions](examples/section2.py)
 - Section 3: Natural QUBO Formulations
   - [Section 3.1: The Number Partitioning Problem](examples/section3.1.py)
+  - [Section 3.2: The Max Cut Problem](examples/section3.2.py)
 - Section 4: Creating QUBO Models Using Known Penalties
 - Section 5: Creating QUBO Models Using a General Purpose Approach
---
+++ examples/section3.2.py
@@ -0,0 +1,91 @@
+"""
+Section 3.2 The Max Cut Problem
+Given an undirected graph G(V,E) with a vertex set V and an edge set E, the Max Cut problem seeks to partition V into two sets such that the number of edges between the two sets (considered to be severed by the cut), is a large as possible.
+"""
+
+import dimod
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+
+linear = {1: -2, 2: -2, 3: -3, 4: -3, 5: -2}
+quadratic = {(1, 2): 1, 
+             (1, 3): 1, 
+             (2, 1): 1,
+             (2, 4): 1,
+             (3, 1): 1,
+             (3, 4): 1,
+             (3, 5): 1,
+             (4, 2): 1,
+             (4, 5): 1,
+             (5, 3): 1,
+             (5, 4): 1}
+offset = 0.0
+vartype = dimod.BINARY
+
+# Expected Solution
+#
+#   x = (0, 1, 1, 0, 0)
+#   Hence vertices 2 and 3 are in one set and vertices 1, 4, and 5 are in the other, with a maximum cut value of 5
+
+bqm = dimod.BinaryQuadraticModel(
+    linear, 
+    quadratic, 
+    offset, 
+    vartype)
+sampler = dimod.ExactSolver()
+sample_set = sampler.sample(bqm)
+print "Using ExactSolver()"
+print sample_set
+
+# Using ExactSolver()
+#     1  2  3  4  5 energy num_oc.
+# 4   0  1  1  0  0   -5.0       1
+# 8   0  0  1  1  0   -5.0       1
+# 9   1  0  1  1  0   -5.0       1
+# 11  0  1  1  1  0   -5.0       1
+# 14  1  0  0  1  0   -5.0       1
+# ...
+# 0   0  0  0  0  0    0.0       1
+# ['BINARY', 32 rows, 32 samples, 5 variables]
+
+print '#'*80
+
+sampler = dimod.SimulatedAnnealingSampler()
+sample_set = sampler.sample(bqm)
+print "Using SimulatedAnnlearingSampler()"
+print sample_set
+
+# Using SimulatedAnnlearingSampler()
+#    1  2  3  4  5 energy num_oc.
+# 0  1  0  1  1  0   -5.0       1
+# 1  1  0  0  1  0   -5.0       1
+# 2  0  0  1  1  0   -5.0       1
+# 3  1  0  0  1  1   -5.0       1
+# 4  0  1  1  1  0   -5.0       1
+# 5  1  0  0  1  1   -5.0       1
+# 6  1  0  1  1  0   -5.0       1
+# 7  0  1  1  0  1   -5.0       1
+# 8  1  0  0  1  1   -5.0       1
+# 9  1  0  0  1  1   -5.0       1
+# ['BINARY', 10 rows, 10 samples, 5 variables]
+
+print '#'*80
+
+sampler = EmbeddingComposite(DWaveSampler())
+esponse = sampler.sample(bqm, num_reads=50)
+print "Using DWaveSampler()"
+print sample_set
+
+# Using DWaveSampler()
+#    1  2  3  4  5 energy num_oc.
+# 0  0  0  1  1  0   -5.0       1
+# 1  0  1  1  0  1   -5.0       1
+# 2  1  0  0  1  0   -5.0       1
+# 3  1  0  0  1  1   -5.0       1
+# 4  0  0  1  1  0   -5.0       1
+# 5  1  0  0  1  1   -5.0       1
+# 6  0  1  1  1  0   -5.0       1
+# 7  1  0  1  1  0   -5.0       1
+# 8  0  1  1  0  1   -5.0       1
+# 9  0  1  1  0  1   -5.0       1
+# ['BINARY', 10 rows, 10 samples, 5 variables]
